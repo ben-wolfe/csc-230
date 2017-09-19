9c6f1a52790dcbec7921fe2ff8ce8d09e519f161
Ben Wolfe <wolfeben16@gmail.com>
2017-09-19 13:49:55 -0700

    Add input error handling

--- Assignments/A2/bitFact.c
+++ Assignments/A2/bitFact.c
@@ -1,6 +1,6 @@
 /*
  * CSC 230 - Intro to Computer Architecture
- * Assignment 1
+ * Assignment 2
  *
  * Author: Ben Wolfe
  * Student Number: V00205547
@@ -14,78 +14,92 @@
 #include <stdio.h>
 
 #define SIZE_INT 16
-#define TEST_VALUE 3
-#define DIVISOR 2
 
 void printBitArray(unsigned char theBits[SIZE_INT]) {
+/* printBitArray takes in an array of unsigned characters and prints out 
+   the contents at each index */
 
+	printf("0b"); // Leading 0b to denote binary number
+	
 	int i = 0;
-	printf("0b");
 	for(i = 0; i < SIZE_INT; i++) {
 		printf("%d", theBits[i]);
 	}
+
 	printf("\n");
 }
 
 void toBits(unsigned short value, unsigned char inBits[SIZE_INT]) {
+/* toBits takes in an unsigned short value and puts the individual bits
+   in seperate indecies of an array */
 
-	
-	int bit_position = 0;
+	// Masking variables
 	unsigned short mask;
 	unsigned short masked_number;
 	unsigned short shifted_number;
-
+	
+	int bit_position = 0;
 	for(bit_position = 0; bit_position < SIZE_INT; bit_position++) {
 		mask = 1 << bit_position;
 		masked_number = mask & value;
 		shifted_number = masked_number >> bit_position;
 		inBits[SIZE_INT - bit_position - 1] = shifted_number;
 	}
-
 }
 
 unsigned short factorial(unsigned short num) {
-
+	
+	// Factorial returns 1 at the base case of 0
 	if(num == 0) {
 		return 1;
-	} 
+	}
 
 	return num * factorial(num-1);
 }
 
 int main(int argc, char* argv[]){
 
-	int user_input = 0;
 	unsigned short conversion_value;
 	unsigned char another_conversion = '\0';
 	unsigned char inBits[SIZE_INT];
 	unsigned short factorial_value = 0;
-
+	
+	// Keep the program running until the user enters 'n'
 	while(1) {
 	
-		printf("Input a positive integer: ");
-		user_input = scanf("%hd", &conversion_value);
+		printf("\nInput a positive integer: ");
+		scanf("%hd", &conversion_value);
 
-		if(user_input != 1) {
-			printf("Invalid input.  Enter a integer between 0 and 65536\n");
+		// Check that the user input is within a valid range
+		if(conversion_value < 0) {
+			printf("Input cannot be negative. Enter a new value\n");
 			continue;
-		}
-	
+		} else if (conversion_value > 8) {
+			printf("Input cannot be larger than 8.  Enter a new value\n");
+			continue;
+		} 
+
+		// Perform the calculation and bit manipulation
 		factorial_value = factorial(conversion_value);
 		printf("   %d Factorial = %d", conversion_value, factorial_value);
 		toBits(factorial_value, inBits);
 		printf(" or ");
 		printBitArray(inBits);
+		printf("\n");
 
+		// Check for program execution
 		printf("Do another converion (y/n): ");
-		user_input = scanf("%s", &another_conversion);
-
+		scanf("%s", &another_conversion);
+		
 		if(another_conversion == 'y') {
 			continue;
 		} else if (another_conversion == 'n') {
+			printf("Program ending");
 			break;
-		} 
-
+		} else {
+			printf("Invalid entry - program ending\n");
+			break;
+		}
 	}
 
 	return 0;
